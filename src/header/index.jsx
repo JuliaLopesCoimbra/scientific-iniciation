216809dc58218a29b693c6a3695444ee22171af1
Júlia Cristina Lopes Coimbra <114704249+JuliaLopesCoimbra@users.noreply.github.com>
2024-11-04 20:34:54 -0300

    agr

--- src/header/index.jsx
+++ src/header/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import { Fragment, useState } from 'react';
+import {  useState } from 'react';
 import { Dialog,Popover } from '@headlessui/react'
 import {
   Bars3Icon,
